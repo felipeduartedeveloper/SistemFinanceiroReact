f0f4150691e871299c5aca53eb7d882351cfb1e7
Felipe Duarte <felipesilvapac@gmail.com>
2021-10-10 23:13:45 -0300

    chore: style color categories

--- src/components/TableItem/styles.ts
+++ src/components/TableItem/styles.ts
@@ -6,3 +6,12 @@ export const TableColumn = styled.td`
   padding: 10px 0;
 
 `;
+
+export const Category = styled.div<{ color: string }>`
+  display: inline-block;
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: #FFF;
+  background-color: ${props => props.color };
+
+`;
